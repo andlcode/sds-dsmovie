751c9624e939d12108e2af5449191cf4cff7df64
Andre Luiz <and969696@outlook.com>
2022-03-09 01:30:19 -0300

    add routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form() {
+
+    return(
+        <h1>luffy</h1>
+    );
+
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing() {
+
+    return(
+        <h1>goku</h1>
+    );
+
+}
+
+export default Listing;
